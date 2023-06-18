c4a922c63900b476ac3192084c9c9e9d778ddc01
Yana Gracheva <115341511+GrachevaYana@users.noreply.github.com>
2023-06-18 20:35:54 +0300

    ДЗ к 2 семинару

---
+++ 1_s11_fibonaccy2.py
@@ -0,0 +1,14 @@
+def get_fib_ind(n):
+    ind = 0
+    a, b = 0, 1
+    while b <= n:
+        ind += 1
+        a, b = b, a + b
+        if a == n:
+            return ind
+    return -1
+print(
+    get_fib_ind(
+        int(input())
+    )
+)
